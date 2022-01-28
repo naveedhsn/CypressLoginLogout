f4326d15f4a5fc8cf6cf6254a1df554db3a38665
naveedhsn <naveedhossaiin@gmail.com>
2022-01-29 02:06:12 +0600

    initial

---
+++ cypress/integration/login-out.js
@@ -0,0 +1,48 @@
+/// <reference types = "cypress"/>
+/// <reference types="cypress-xpath" />
+describe('Cypress Testing For Login-Logout', function () {
+    it('Login', function () {
+        cy.visit('https://multapplied-staging.w3engineers.com/')
+
+        cy.get('#email').type('rakib@w3engineers.com')
+
+        cy.get('#password').type('11v&EhWkF&')
+
+        cy.get('.el-button > span').click()
+
+        cy.wait(5000)
+        cy.get('button > svg').click()
+    })
+
+    it ('Checking host', function(){
+        cy.get(':nth-child(2) > .el-submenu > .el-submenu__title > .el-submenu__icon-arrow').click()
+    })
+
+    it ('Checking Aggregators', function(){
+        cy.get(':nth-child(2) > .el-submenu > .el-menu > :nth-child(1) > :nth-child(1) > a > .el-menu-item > .el-tooltip > .menu-text').click()
+        cy.wait(5000)
+    })
+
+    it ('Checking Bonds', function(){
+        cy.get('.myBtn > :nth-child(3) > :nth-child(1) > a > .el-menu-item > .el-tooltip').click()
+    })
+
+    it ('Checking Spaces', function(){
+        cy.get('.myBtn > :nth-child(4) > :nth-child(1) > a > .el-menu-item > .el-tooltip').click()
+    })
+
+    it ('Checking Policies', function(){
+        cy.get(':nth-child(5) > .el-submenu > .el-submenu__title > .el-submenu__icon-arrow').click()
+    })
+
+    it ('Land on Dashboard', function(){
+        cy.get(':nth-child(1) > :nth-child(1) > .router-link-active > .el-menu-item > .el-tooltip').click()
+    })
+
+    it ('Signing Out', function(){
+        cy.get('.avatar-wrapper').click()
+        cy.xpath('/html/body/ul/li[2]').within(() => {cy.xpath('/html/body/ul/li[2]')}).click() 
+        Cypress.on('uncaught:exception', err => !err.message.includes('ResizeObserver loop limit exceeded'))
+       
+    })
+})
